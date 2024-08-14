f58715c1c6b9e8741ea4dcef8862a3c1c71bc24c
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-08-14 15:22:51 -0700

    Update TransparentSupplyChainTest.sol

--- TransparentSupplyChain/tests/TransparentSupplyChainTest.sol
+++ TransparentSupplyChain/tests/TransparentSupplyChainTest.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.26;
 
 import "../contracts/TransparentSupplyChain.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
